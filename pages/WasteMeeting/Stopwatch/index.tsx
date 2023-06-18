f1f3c313bdd6cc462f4fa7e9ec752c46d1b418f7
arubaito <asdfghjk0330@gmail.com>
2023-06-18 11:08:54 +0900

    コスト計算機能をとりあえずざっくり追加

--- pages/WasteMeeting/Stopwatch/index.tsx
+++ pages/WasteMeeting/Stopwatch/index.tsx
@@ -1,16 +1,72 @@
 import { useRouter } from "next/router";
+import { useRef, useState } from "react";
 
 export default function Stopwatch() {
-    
-    // 遷移元から値を受け取る
+
+    // ストップウォッチ
+    const [time, setTime] = useState<number>(0);
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+    const interValRef = useRef<number | undefined>(undefined);
+
+    // コスト計算
     const router = useRouter();
-    
+    const per1hourCost: number = Number(router.query.cost1) + Number(router.query.cost2) + Number(router.query.cost3) + Number(router.query.cost4);
+    const per1minCost: number = Math.round(per1hourCost / 60);
+    const per1sCost: number = Math.round(per1minCost / 60); // TODO:1min辺り0¥だとコストが0になる
+    const [cost, setCost] = useState<number>(0);
+
+
+    // START
+    function handleStart(): void {
+        setIsRunning(true);
+        interValRef.current = window.setInterval(() => {
+            setTime(prevTime => prevTime + 1000); // 時刻計算
+            setCost(cost => cost + per1sCost); // コスト計算
+        }, 1000)
+    }
+
+    // STOP
+    function handlePause(): void {
+        clearInterval(interValRef.current);
+        setIsRunning(false);
+    }
+
+    // RESET
+    function handleReset(): void {
+        clearInterval(interValRef.current);
+        setIsRunning(false);
+        setTime(0);
+        setCost(0);
+    }
+    // 時刻表示用の変換
+    const seconds = `0${Math.floor(time / 1000) % 60}`.slice(-2);
+    const minutes = `0${Math.floor(time / 60000) % 60}`.slice(-2);
+    const hours = `0${Math.floor(time / 3600000) % 60}`.slice(-2);
+
+
     return (
         <>
-        {router.query.cost1}
-        {router.query.cost2}
-        {router.query.cost3}
-        {router.query.cost4}
+            <h1>Your Meeting Time</h1>
+            <div>
+                <div>{hours}:{minutes}:{seconds}</div>
+                <div id="buttons">
+                    {isRunning ? (
+                        <button onClick={handlePause}>PAUSE</button>
+                    ) : (
+                        <button onClick={handleStart}>START</button>
+                    )}
+                    <button onClick={handleReset}>RESET</button>
+                </div>
+            </div>
+            <h1>Participants Cost</h1>
+            <div>{cost}¥</div>
+            <h3>Per 1H Cost</h3>
+            <div>{per1hourCost}¥</div>
+            <h3>Per 1MIN Cost</h3>
+            <div>{per1minCost}¥</div>
+            <h3>Per 1S Cost</h3>
+            <div>{per1sCost}¥</div>
+
         </>
 
     );
