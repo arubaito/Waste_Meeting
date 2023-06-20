361236951444e221c97f181ef7eae86f69adaa78
arubaito <asdfghjk0330@gmail.com>
2023-06-20 10:38:16 +0900

    トップメニューの見た目を修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,26 @@
 import Link from "next/link";
+import styles from "styles/C01_TopMenu.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStopwatch, faCoins } from "@fortawesome/free-solid-svg-icons";
 
-export default function Home() {
+export default function TopMenu() {
   return (
     <>
-      <div>
-        <div>
-          <Link href="/WasteMeeting/RegisterUser">Waste Meeting</Link>
-        </div>
-        <div>
-          <Link href="/SimpleStopwatch">Simple Stopwatch</Link>
+      <div className={styles.menuContainer}>
+        <h1>メニュー</h1>
+        <div className={styles.menuItems}>
+          <div className={styles.menuItem}>
+            <Link href="/WasteMeeting/RegisterUser">
+              <FontAwesomeIcon icon={faCoins} />
+              <span>Waste Meeting</span>
+            </Link>
+          </div>
+          <div className={styles.menuItem}>
+          <Link href="/SimpleStopwatch">
+            <FontAwesomeIcon icon={faStopwatch} />
+            <span>Simple Stopwatch</span>
+          </Link>
+          </div>
         </div>
       </div>
     </>
