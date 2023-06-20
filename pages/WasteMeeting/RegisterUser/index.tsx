37efa0dfd81b30064d0a894cc4b1dec43ee92a54
arubaito <asdfghjk0330@gmail.com>
2023-06-20 13:26:12 +0900

    (W01)参加者登録画面の見た目を修正

--- pages/WasteMeeting/RegisterUser/index.tsx
+++ pages/WasteMeeting/RegisterUser/index.tsx
@@ -2,12 +2,15 @@ import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
 import { useRef } from "react";
+import styles from "styles/W01_RegisterUser.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYenSign } from "@fortawesome/free-solid-svg-icons";
 
 /**
  * ユーザのコスト登録
  */
 export default function RegisterUser() {
-    
+
     // 翻訳ファイル
     const { t } = useTranslation('common')
 
@@ -20,46 +23,73 @@ export default function RegisterUser() {
     // 入力したコストを画面遷移先に渡す
     const clickHandler = () => {
         router.push({
-            pathname:"/WasteMeeting/Stopwatch", 
+            pathname: "/WasteMeeting/Stopwatch",
             query: {
-                cost1: cost1.current, 
-                cost2: cost2.current, 
-                cost3: cost3.current, 
-                cost4: cost4.current, 
+                cost1: cost1.current,
+                cost2: cost2.current,
+                cost3: cost3.current,
+                cost4: cost4.current,
             }
         });
     }
-    
-    
+
+
     return (
         <>
-            <h1>{t("W01_register_participants_cost")} (/ 1h)</h1>
-            <div>
+            <div className={styles.contentsContainer}>
                 <form >
-                    <div>{t("W01_participant")} 1 : <input type="number" onChange={(e) => cost1.current = Number(e.target.value)} /> ¥</div>
-                    <div>{t("W01_participant")} 2 : <input type="number" onChange={(e) => cost2.current = Number(e.target.value)} /> ¥</div>
-                    <div>{t("W01_participant")} 3 : <input type="number" onChange={(e) => cost3.current = Number(e.target.value)} /> ¥</div>
-                    <div>{t("W01_participant")} 4 : <input type="number" onChange={(e) => cost4.current = Number(e.target.value)} /> ¥</div>
+                    <div className={styles.inputContainer}>
+                        <div>
+                            <span className={styles.participantText}>{t("W01_participant")} 1 :</span>
+                            <input type="number" onChange={(e) => cost1.current = Number(e.target.value)} />
+                            <span className={styles.currencyUnit}>
+                                <FontAwesomeIcon icon={faYenSign} />
+                            </span>
+                        </div>
+                        <div>
+                            {t("W01_participant")} 2 :
+                            <input type="number" onChange={(e) => cost2.current = Number(e.target.value)} />
+                            ¥
+                        </div>
+                        <div>
+                            {t("W01_participant")} 3 : <input type="number" onChange={(e) => cost3.current = Number(e.target.value)} />
+                            ¥
+                        </div>
+                        <div>
+                            {t("W01_participant")} 4 : <input type="number" onChange={(e) => cost4.current = Number(e.target.value)} />
+                            ¥
+                        </div>
+                    </div>
+                    <button >+</button>
                 </form>
-            </div>
-            <div>
-                <button onClick={clickHandler} >{t("W01_go")}</button>
+                <div className={styles.buttons}>
+                    <button onClick={clickHandler} >{t("W01_go")}</button>
+                </div>
             </div>
         </>
 
     );
 }
 
+
+
+
+
+
+
+
 /**
  * 
  * @param locale: next-i18nextの設定 
  */
-export async function getStaticProps({ locale }: { locale: any}) {
+export async function getStaticProps({ locale }: { locale: any }) {
     return {
-      props: {
-        ...(await serverSideTranslations(locale, [
-          'common',
-        ])),
-      },
+        props: {
+            ...(await serverSideTranslations(locale, [
+                'common',
+            ])),
+        },
     }
-  }
+}
+
+
